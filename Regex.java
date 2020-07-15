b78dad4a346bf03a66fe85297788feff9659f3b7
Fernando S Nascimento <fernansn5@gmail.com>
2020-07-15 14:00:26 -0300

    Regex criado

---
+++ Regex.java
@@ -0,0 +1,25 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex 
+{
+    public static boolean EstaPresente (String contem, String contida) 
+    {
+			return createPatternToValue(contida).matcher(contem).matches();
+	}
+	
+    private static Pattern createPatternToValue(final String value) 
+    {
+		return Pattern.compile("([0-9]*[0-9]:[0-5][0-9])" + "\\s(" + value + ":)(.*?)");
+	}
+	
+    public static Matcher getMatcherForSentence(final String sentence, final String value) 
+    {
+		return createPatternToValue(value).matcher(sentence);
+	}
+	
+    public static Matcher createPatternForSetence(final String pattern, final String sentence) 
+    {
+		return Pattern.compile(pattern).matcher(sentence);
+	}
+}
